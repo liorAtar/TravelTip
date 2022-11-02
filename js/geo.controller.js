0b1bdffc2b2dc948d3cb0d588031ab9380c95907
barzaken <bar.zaken@boyar.org.il>
2022-11-02 16:05:50 +0200

    geo-location added

---
+++ js/geo.controller.js
@@ -0,0 +1,18 @@
+window.onSearchLocation = onSearchLocation
+
+
+const GEO_API = 'AIzaSyAn_Ij60OAOhlvDr-QtD_Ih-JHFtzPdV6o'
+
+function onSearchLocation(){
+    let location = document.querySelector('.search-input').value
+    let address = location.replace(/\s/g, '%')
+    console.log(address)
+    getLocation(address)
+}
+
+function getLocation(address){
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GEO_API}`
+    const prm1 = fetch(url)
+    .then(res => console.log(res.url))
+
+}
